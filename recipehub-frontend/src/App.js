eff1084a2da741916d9754f06575633e533653ab
HarmanTCXR <154282449+HarmanTCXR@users.noreply.github.com>
2025-06-08 11:35:56 -0700

    Added frontend

---
+++ recipehub-frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import HomePage from './pages/HomePage/HomePage'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;

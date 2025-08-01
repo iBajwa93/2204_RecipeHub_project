35439095ed2d510d77ed9c23ec67c4be63b3bdc9
HarmanTCXR <154282449+HarmanTCXR@users.noreply.github.com>
2025-08-01 09:50:36 -0700

    all features implement, some bug fixing now

--- recipehub-frontend/src/components/banned/Banned.js
+++ recipehub-frontend/src/components/banned/Banned.js
@@ -1,4 +1,5 @@
 import React from "react";
+import miniChef from '../../assets/icons/minichef.png';
 
 const BannedModal = () => {
   return (
@@ -11,20 +12,26 @@ const BannedModal = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        fontFamily: 'HindLight',
+        letterSpacing: '1px',
         zIndex: 9999,
       }}
     >
+       
       <div
         style={{
           backgroundColor: "white",
           padding: "30px",
           borderRadius: "8px",
           maxWidth: "400px",
+          fontFamily: 'HindLight',
           textAlign: "center",
+          letterSpacing: '1px',
           boxShadow: "0 0 10px rgba(0,0,0,0.25)",
           userSelect: "none",
         }}
       >
+         <img src={miniChef}/>
         <h2>You have been banned</h2>
         <p>Your account has been suspended. Please contact support if you believe this is a mistake.</p>
       </div>
